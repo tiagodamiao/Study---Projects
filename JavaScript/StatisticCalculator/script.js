00f37471894445ab96bec8a73e99cbac2c6a4020
Tiago Damião <tiagomigueldamiao@gmail.com>
2025-05-23 16:34:00 +0100

    updates

--- JavaScript/StatisticCalculator/script.js
+++ JavaScript/StatisticCalculator/script.js
@@ -17,9 +17,34 @@ const getMode = (array) => {
   if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
-  const highest = Object.keys(counts);
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
+
+const getStandardDeviation = (array) => {
+  const variance = getVariance(array);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -28,7 +53,16 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
 
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+
 }

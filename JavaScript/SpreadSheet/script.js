cf005e79893f5d48237366948bd8d094f69bed56
Tiago Damião <tiagomigueldamiao@gmail.com>
2025-06-07 18:22:25 +0100

    updates

--- JavaScript/SpreadSheet/script.js
+++ JavaScript/SpreadSheet/script.js
@@ -1,8 +1,29 @@
-
+const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+
+const median = nums => {
+  const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
+  return isEven(length)
+    ? average([sorted[middle], sorted[middle + 1]])
+    : sorted[Math.ceil(middle)];
+}
+
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median
+}
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
+
+const evalFormula = (x, cells) => {
+  const idToText = id => cells.find(cell => cell.id === id).value;
+
+}
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -21,7 +42,16 @@ window.onload = () => {
       input.type = "text";
       input.id = letter + number;
       input.ariaLabel = letter + number;
+      input.onchange = update;
       container.appendChild(input);
     })
   })
+}
+
+const update = event => {
+  const element = event.target;
+  const value = element.value.replace(/\s/g, "");
+  if (!value.includes(element.id) && value.startsWith('=')) {
+
+  }
 }

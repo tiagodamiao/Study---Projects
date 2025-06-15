950277d535da435ed103698f412ef14eec72c81e
Tiago Damião <tiagomigueldamiao@gmail.com>
2025-06-15 22:18:11 +0100

    updates

--- JavaScript/SpreadSheet/script.js
+++ JavaScript/SpreadSheet/script.js
@@ -22,6 +22,11 @@ const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).
 
 const evalFormula = (x, cells) => {
   const idToText = id => cells.find(cell => cell.id === id).value;
+  const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?)/gi;
+  const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = num => character => idToText(character + num);
+  const addCharacters = character1 => character2 => num => charRange(character1, character2).map(elemValue(num));
+  const rangeExpanded = x.replace(rangeRegex, (_match, char1, num1, char2, num2) => rangeFromString(num1, num2).map(addCharacters(char1)(char2)));
 
 }
 
